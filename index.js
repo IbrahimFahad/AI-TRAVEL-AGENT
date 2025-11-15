c5c993417d820ceff9fc5f95b50b3c207af5d576
Ibrahim Fahad <Ibrazoom68@gmail.com>
2025-11-15 19:20:16 +0300

    html css done

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
-    dangerouslyAllowBrowser: true
-})
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.querySelector(".btn");
+  btn.addEventListener("click", () => {
+    window.location.href = "select.html";
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.querySelector(".btn-2");
+  btn.addEventListener("click", () => {
+    window.location.href = "result.html";
+  });
+});
+
+let count = 1;
+
+const value = document.getElementById("value");
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
+
+plus.addEventListener("click", () => {
+  count++;
+  value.textContent = count;
+});
+
+minus.addEventListener("click", () => {
+  if (count > 0) {
+    count--;
+    value.textContent = count;
+  }
+});
